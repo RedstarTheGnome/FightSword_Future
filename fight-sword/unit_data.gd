c801fd294dff79e92f28ea4bd5ae0bb3de344fcd
Jordan <Jordanredpath@aol.com>
2026-08-07 10:48:31 +0100

    unit data created

---
+++ fight-sword/unit_data.gd
@@ -0,0 +1,18 @@
+class_name UnitData
+extends Resource
+
+@export var unit_type_id: String = ""
+@export var dispay_name: String = "Tactical Squad"
+@export var movement_range: float = 6.0
+@export var max_health: int = 10
+@export var damage: int = 2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
