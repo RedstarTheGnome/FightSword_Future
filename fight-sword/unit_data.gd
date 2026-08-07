5a73ed5fc231949a496d84a9330a466e22410b92
Jordan <Jordanredpath@aol.com>
2026-08-07 12:02:28 +0100

    tidyed unit data. tested boys unit working

--- fight-sword/unit_data.gd
+++ fight-sword/unit_data.gd
@@ -6,13 +6,3 @@ extends Resource
 @export var movement_range: float = 6.0
 @export var max_health: int = 10
 @export var damage: int = 2
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
